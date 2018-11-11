dff3b68892b1c48e4befb1354a063f1505f1a400
zlargon <zlargon1988@gmail.com>
2018-11-11 05:39:21 -0500

    [lifecycle] implement PING API

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Samsung Lifecycle
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+
+  // PING
+  // https://smartthings.developer.samsung.com/develop/guides/smartapps/lifecycles.html#PING
+  if (req.body.lifecycle === 'PING') {
+    res.status(200).json({
+      pingData: req.body.pingData
+    });
+  }
+
+});
+
 module.exports = router;
