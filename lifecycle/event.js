7e32353d63c256542a40a5684a4f2270db5169ab
zlargon <zlargon1988@gmail.com>
2018-11-19 16:59:16 -0500

    [lifecycle][event] ready for Parse integration

--- lifecycle/event.js
+++ lifecycle/event.js
@@ -1,7 +1,17 @@
 module.exports = async ({ eventData: data }) => {
   for (const event of data.events) {
     console.json(event.deviceEvent);
+
+    const data = {
+      timestamp: Date.now()
+      deviceId: event.deviceEvent.deviceId,
+      hubId: event.deviceEvent.locationId,
+      deviceType: event.deviceEvent.capability,
+      value: event.deviceEvent.value
+    };
   }
+
+  // TODO: upload to Parser
 
   return {
     eventData: {}
